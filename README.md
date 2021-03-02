4a6a0f905dfdd109982b55f054d07164f3ddd8bc
bailidf <bailidf@asiainfo.com>
2021-03-02 15:55:17 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,4 @@
+
+### 本项目为基于WxJava的微信小程序后端程序，基于Spring Boot构建，实现微信小程序后端开发功能，支持多个小程序。
+基于binarywang的小程序demo改造：https://github.com/binarywang/weixin-java-miniapp-demo
+
---
+++ wx-application-mini/src/main/java/com/github/folderbai/wx/mini/WxMaApplication.java
@@ -0,0 +1,15 @@
+package com.github.folderbai.wx.mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://github.com/binarywang">Binary Wang</a>
+ */
+@SpringBootApplication
+public class WxMaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxMaApplication.class, args);
+    }
+}
